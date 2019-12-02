9633ce3591b66c3ce645d8c0d778bc6a86b96891
sambapython <sambapython@gmail.com>
2019-12-02 08:17:52 +0530

    2019-12-02 unittest cases

--- warehouse/api/tests.py
+++ warehouse/api/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class APITest(TestCase):
+	@classmethod
+	def setUpClass(cls):
+		user = User.objects.create_user(username="user1",password="user1")
+		cls.client = APIClient()
+		
+		
+	@classmethod
+	def tearDownClass(cls):
+		cls.client.logout()
+
+	def test_CreateCategory(self):
+		resp = self.client.login(username="user1",password="user1")
+		resp = self.client.post("/api/categories/",data={"name":"apicat"})
+		self.assertTrue(resp.status_code in [200,201])
+		resp = self.client.get("/api/categories/")
+		self.assertTrue(resp.status_code==200)
+		data = resp.json()
+		cat = data[0]
+		self.assertTrue(cat["name"]==apicat)
+	# n number of test cases
+
--- warehouse/product/tests.py
+++ warehouse/product/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from product.models import Category,Product
 
 # Create your tests here.
+
+class CategoryTest(TestCase):
+
+    def test_create(self):
+        cat = Category(name="cat1")
+        cat.save()
+        cats = Category.objects.all()
+        error = "test_create failed"
+        if cats:
+            cat_name = cats[0].name 
+            self.assertEqual("cat1",cat_name,error)
+        else:
+            self.assertEqaul(1,2,error)
+class ProductTest(TestCase):
+    def setUp(self):
+        cat = Category(name="cat1")
+        cat.save()
+        cats = Category.objects.all()
+        cat = cats[0]
+        pro = Product(name="prod1",category=cat,description="desc1")
+        pro.save()
+        self.prod_id=pro.id
+
+    def test_product_update_name(self):
+        pro = Product.objects.get(id=self.prod_id)
+        pro.name="Update prod1"
+        pro.save()
+        pro = Product.objects.get(id=self.prod_id)
+        self.assertTrue(pro.name=="Update prod1","test_product_update_name failed")
+    def test_product_update_description(self):
+        pro = Product.objects.get(id=self.prod_id)
+        pro.description="Update prod1"
+        pro.save()
+        pro = Product.objects.get(id=self.prod_id)
+        self.assertTrue(pro.description=="Update prod1","test_product_update_description failed")
+    def tearDown(self):
+        pro = Product.objects.all()
+        [i.delete() for i in pro]
+
